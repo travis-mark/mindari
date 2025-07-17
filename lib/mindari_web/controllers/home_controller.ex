a7e31c357471bd7b108b872aae1b66b8b799db30
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2025-07-17 19:45:36 -0400

    Dev dashboard link

--- lib/mindari_web/controllers/home_controller.ex
+++ lib/mindari_web/controllers/home_controller.ex
@@ -4,6 +4,7 @@ defmodule MindariWeb.HomeController do
   def index(conn, _params) do
     conn
     |> assign(:page_title, "Home Page")
+    |> assign(:dev_routes_enabled, Application.get_env(:mindari, :dev_routes, false))
     |> render(:index)
   end
 end
