480003530ef007bbeb46ab98e7e5215dece03f81
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2025-07-31 18:58:21 +0100

    Then -> Home link

--- test/mindari_web/controllers/onthisday_controller_test.exs
+++ test/mindari_web/controllers/onthisday_controller_test.exs
@@ -2,7 +2,6 @@ defmodule MindariWeb.OnThisDayControllerTest do
   use MindariWeb.ConnCase
 
   import Mindari.AccountsFixtures
-  alias Mindari.Accounts
 
   setup do
     %{unconfirmed_user: unconfirmed_user_fixture(), user: user_fixture()}
@@ -19,6 +18,18 @@ defmodule MindariWeb.OnThisDayControllerTest do
       html = html_response(conn, 200)
 
       assert html =~ "On This Day"
+    end
+
+    test "Has link to home page", %{conn: conn, user: user} do
+      user = set_password(user)
+      conn =
+        post(conn, ~p"/login", %{
+          "user" => %{"email" => user.email, "password" => valid_user_password()}
+        })
+      conn = get(conn, ~p"/then")
+      html = html_response(conn, 200)
+
+      assert html =~ ~r/href="\/"/
     end
   end
 end
